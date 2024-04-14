a287e9fade82eeee92e0278885490ff27a74e3b2
Cheng Ho Ming <eric310@connect.hku.hk>
2024-04-14 17:52:19 +0800

    Bug: initial box coordinates same results in 'not overlap' despite do

--- Assignment2/P2/2.cpp
+++ Assignment2/P2/2.cpp
@@ -20,18 +20,18 @@ bool mergeBoundingBoxes(int & x1, int & y1, int & w1, int & h1, int x2, int y2, 
     // test cases may provide different inputs, hence we need to check
     // which corner is closer to the top left corner (0,0)
     // x2 > x1 : check if the second box is more to the right
-    if ((x1_ > x2) && (y1_ > y2) && (x1 < x2) && (y1 < y2)){
+    if ((x1_ > x2) && (y1_ > y2) && (x1 <= x2) && (y1 <= y2)){
         overlap = true;
     }
     // if the second box is more to the top left corner,
     // compare the bottom right corner of the first box with the top left corner of the second box
-    else if ((x2_ > x1) && (y2_ > y1) && (x1 > x2) && (y1 > y2)){
+    else if ((x2_ > x1) && (y2_ > y1) && (x1 >= x2) && (y1 >= y2)){
         overlap = true;
     }
-    else if ((x1_ > x2) && (y2_ > y1) && (x1 < x2) && (y1 > y2)){
+    else if ((x1_ > x2) && (y2_ > y1) && (x1 <= x2) && (y1 >= y2)){
         overlap = true;
     }
-    else if ((x2_ > x1) && (y1_ > y2) && (x1 > x2) && (y1 < y2)){
+    else if ((x2_ > x1) && (y1_ > y2) && (x1 >= x2) && (y1 <= y2)){
         overlap = true;
     }
     // the width and height of the merged box
